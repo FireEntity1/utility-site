c498f68b4a302be1cadc1397f1796767c2664fd2
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-07-31 19:58:03 -0600

    Basic calculation

---
+++ index.js
@@ -0,0 +1,59 @@
+var num1 = null;
+var num2 = null;
+var output = null;
+var op = "";
+
+function calc() {
+    num1 = parseFloat(document.getElementById('input1').value);
+    num2 = parseFloat(document.getElementById('input2').value);
+    switch (op) {
+        case "+":
+            output = num1 + num2;
+            document.getElementById('input1').value = output;
+            document.getElementById('input2').value = "";
+            num1 = output;
+            break;
+        case "-":
+            output = num1 - num2;
+            document.getElementById('input1').value = output;
+            document.getElementById('input2').value = "";
+            num1 = output;
+            break;
+        case "*":
+            output = num1 * num2;
+            document.getElementById('input1').value = output;
+            document.getElementById('input2').value = "";
+            num1 = output;
+            break;
+        case "/":
+            output = num1 / num2;
+            document.getElementById('input1').value = output;
+            document.getElementById('input2').value = "";
+            num1 = output;
+            break;
+    }
+}
+
+function add() {
+    op = "+";
+    calc();
+}
+
+function sub() {
+    op = "-";
+    calc();
+}
+
+function mult() {
+    op = "*";
+    calc();
+}
+
+function div() {
+    op = "/";
+    calc();
+}
+
+function equal() {
+    calc();
+}
